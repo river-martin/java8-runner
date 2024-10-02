b264cadca5a4c039526e772b7358cbb79060285a
river-martin <rivermartin128@gmail.com>
2024-10-02 13:48:09 +0200

    Take regex file path

--- Java8Runner.java
+++ Java8Runner.java
@@ -6,20 +6,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Java8Runner {
-  public static void main(String[] args) {
-    if (args.length != 2) {
-      System.err.println("Usage: Java8Runner <pattern> <input_file_name>");
-      System.exit(1);
-    }
-    File file = new File(args[1]);
+
+  /**
+   * Read a file and return its content as a string.
+   * 
+   * @param path The path to the file.
+   * @return The content of the file.
+   */
+  private static String readFile(String path) {
+    File file = new File(path);
     String input = null;
     try {
       input = new String(Files.readAllBytes(file.toPath()));
     } catch (IOException e) {
-      System.err.println("Error reading file: " + args[1]);
+      System.err.println("Error reading file: " + path);
       System.exit(1);
     }
-    Matcher matcher = Pattern.compile(args[0]).matcher(input);
+    return input;
+  }
+
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.err.println("Usage: Java8Runner <regex_file_path> <input_file_path>");
+      System.exit(1);
+    }
+    String regex = readFile(args[0]);
+    String input = readFile(args[1]);
+    Matcher matcher = Pattern.compile(regex).matcher(input);
     if (matcher.find()) {
       System.out.println("[match_1]\n");
       MatchResult result = matcher.toMatchResult();
@@ -37,6 +50,5 @@ public class Java8Runner {
     } else {
       System.out.println("# no match");
     }
-
   }
 }
